e3240052d494abfafb9e88c5924ca82ac886d18a
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-04-22 12:09:20 -0300

    Criação do formulario de criação do galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -4,4 +4,20 @@ class WarehousesController < ApplicationController
 
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    
+  end 
+
+  def create
+  # Recebe os dados enviados
+  # Cria um novo galpão no banco de dados
+  warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description,
+                                                       :address, :cep, :area)
+  w = Warehouse.new(warehouse_params)
+  w.save()
+
+  #Redireciona par a tela inicial
+    redirect_to root_path
+  end
 end

d2033e68692b8b2e7f1cc0297b00022e4a2227e0
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-04-29 11:22:06 -0300

    Criação do model: product_model + testes + validações + tradução

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,7 @@
 class Supplier < ApplicationRecord
 
+  has_many :product_models
+
   validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, 
             :email, :phone_number, presence: true
   validates :registration_number, uniqueness: true
